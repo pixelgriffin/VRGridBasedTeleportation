049ce2852c6692cee655caec76e738922f88b82a
pixelgriffin <navarrogriffin@gmail.com>
2018-11-26 22:31:16 -0800

    Upgraded beta version and added full locomotion

---
+++ Assets/FullLocomotion.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class FullLocomotion : MonoBehaviour
+{
+    [SteamVR_DefaultAction("Drag", "default")]
+    public SteamVR_Action_Vector2 dragAction;
+
+    public Player player;
+
+    public bool allowHorizontalMotion = false;
+
+    void Update()
+    {
+        Vector3 motion = Vector3.zero;
+
+        foreach (Hand hand in player.hands)
+        {
+            Vector2 input = dragAction.GetAxis(hand.handType);
+            if (input != Vector2.zero)
+            {
+                motion += FlattenVectorZ(player.hmdTransform.forward) * input.y * Time.deltaTime;
+
+                if(allowHorizontalMotion)
+                {
+                    motion += FlattenVectorZ(player.hmdTransform.right) * input.x * Time.deltaTime;
+                }
+            }
+        }
+
+        player.transform.position += motion;
+    }
+
+    private Vector3 FlattenVectorZ(Vector3 vec)
+    {
+        vec.y = 0;
+        return vec;
+    }
+}
